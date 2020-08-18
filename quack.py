94cbd8e3587abb6f00ac40f391d89b985e4b6664
Yakov Shalunov <yakov.shalunov@gmail.com>
2020-08-17 17:38:55 -0700

    Main module will automatically reboot subprocesses.

--- quack.py
+++ quack.py
@@ -1,15 +1,20 @@
 import subprocess
 import sys
+import time
 
 modules = ["fauxclient.py", "base.py", "patrol.py", "graph.py"]
-processes = []
+processes = {}
 try:
     for module in modules:
-        processes.append(subprocess.Popen([sys.executable, module]))
-    for process in processes:
-        process.wait()
+        processes[module] = subprocess.Popen([sys.executable, module])
+    while True:
+        for module, process in processes.items():
+            poll_result = process.poll()
+            if poll_result is not None:
+                processes[module] = subprocess.Popen([sys.executable, module])
+        time.sleep(5)
 except KeyboardInterrupt:
-    for process in processes:
+    for process in processes.values():
         process.terminate()
-    for process in processes:
+    for process in processes.values():
         process.wait()
