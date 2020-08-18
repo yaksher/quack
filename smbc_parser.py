8ced74671eae62437311a0ea0d826c14d7e94635
Yakov Shalunov <yakov.shalunov@gmail.com>
2020-08-18 03:02:08 -0700

    Clean up SMBC pareser.

--- smbc_parser.py
+++ smbc_parser.py
@@ -15,29 +15,23 @@ class SMBCParser(HTMLParser):
     def __init__(self, comic):
         super().__init__()
         self.div = None
-        self.current = comic
-        self.comic = None
-        self.title = None
-        self.after_comic = None
+        self.title = comic
+        self.comic_embed = None
+        self.hover_text = None
+        self.after_comic_embed = None
 
     def parse(self):
         smbc_page = str(requests.get(f"https://www.smbc-comics.com/comic/{self.current}").content)
         self.feed(smbc_page)
-        return (self.current, self.comic, self.title, self.after_comic)
-
-    def get_next(self):
-        smbc_page = str(requests.get(f"https://www.smbc-comics.com/comic/{self.current}").content)
-        self.feed(smbc_page)
-        return self.next
-
-
+        return (self.title, self.comic_embed, self.hover_text, self.after_comic_embed)
+    
     def handle_starttag(self, tag, attrs):
         attrs = dict(attrs)
         if tag == 'div' and 'id' in attrs:
             self.div = attrs['id']
         if self.div == 'cc-comicbody' and tag == 'img' and attrs['id'] == 'cc-comic':
-            self.comic = attrs['src']
-            self.title = attrs['title']
+            self.comic_embed = attrs['src']
+            self.hover_text = attrs['title']
         if self.div == 'aftercomic' and tag == 'img':
-            self.after_comic = attrs['src'][2:-2]
+            self.after_comic_embed = attrs['src'][2:-2]
 
