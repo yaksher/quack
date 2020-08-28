267db9f50131a4bd8cc605dbb86f20402fe4db70
Yakov <yakov.shalunov@gmail.com>
2020-08-27 23:58:50 -0700

    add time to test log.

--- ventbot.py
+++ ventbot.py
@@ -96,8 +96,7 @@ async def on_message_edit(before, after):
 
 @bot.event
 async def on_message(msg):
-    if msg.guild is None:
-        print('received message')
+    print('received message', time.time())
     send_id = msg.author.id
     if msg.guild is None and not send_id in admin_ids_live and send_id != bot.user.id and bot.get_guild(tech_id).get_member(send_id) is not None:
         if not msg.channel in subbed:
