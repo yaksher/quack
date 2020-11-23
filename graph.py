ffa8fbb6bbd0129aeebb92e4ab9eafefc174f9ba
Yakov <yakov.shalunov@gmail.com>
2020-11-22 19:24:08 -0800

    Add parametric graphing to graph module

--- graph.py
+++ graph.py
@@ -23,7 +23,7 @@ async def restart(ctx, *args):
 
 @bot.command()
 async def graph(ctx, *args):#x_bound:str, y_bound = "", *args):
-    in_str = " ".join(args)
+    in_str = " ".join(args).strip("`")
     bound_y = r"y ?= ?\(-?[0-9]+\.?[0-9]*, ?-?[0-9]+\.?[0-9]*\)"
     bound_x = r"x ?= ?\(-?[0-9]+\.?[0-9]*, ?-?[0-9]+\.?[0-9]*\)"
     y_bound = re.findall(bound_y, in_str)
@@ -70,6 +70,90 @@ async def graph(ctx, *args):#x_bound:str, y_bound = "", *args):
     ax.plot(x, y)
     ax.spines['left'].set_position(('data', min(max(ax.get_xlim()[0], 0), ax.get_xlim()[1])))
     ax.spines['bottom'].set_position(('data', min(max(ax.get_ylim()[0], 0), ax.get_ylim()[1])))
+    buf = io.BytesIO()
+    buf.name = "graph.png"
+    fig.savefig(buf, format='png', transparent=True)
+    buf.seek(0)
+    await ctx.send(file=discord.File(buf))
+
+@bot.command()
+async def graph_param(ctx):#, *args):#x_bound:str, y_bound = "", *args):
+    in_str = ctx.message.content[len("?graph_param"):].strip("` ")
+    polar = in_str.startswith("polar")
+    in_str = in_str[5:] if polar else in_str
+    bound_y = r"y ?= ?\(-?[0-9]+\.?[0-9]*, ?-?[0-9]+\.?[0-9]*\)"
+    bound_x = r"x ?= ?\(-?[0-9]+\.?[0-9]*, ?-?[0-9]+\.?[0-9]*\)"
+    bound_t = r"t ?= ?\(-?[0-9]+\.?[0-9]*, ?-?[0-9]+\.?[0-9]*\)"
+    y_bound = re.findall(bound_y, in_str)
+    x_bound = re.findall(bound_x, in_str)
+    t_bound = re.findall(bound_t, in_str)
+    if y_bound:
+        in_str = re.sub(bound_y, lambda y: "", in_str)
+        y_bounds = tuple(float(y) for y in y_bound[0].replace(" ", "")[3:-1].split(","))
+    else:
+        y_bounds = None
+    if x_bound:
+        in_str = re.sub(bound_x, lambda x: "", in_str)
+        x_bounds = tuple(float(x) for x in x_bound[0].replace(" ", "")[3:-1].split(","))
+    else:
+        x_bounds = None
+    if t_bound:
+        in_str = re.sub(bound_t, lambda t: "", in_str)
+        t_bounds = tuple(float(t) for t in t_bound[0].replace(" ", "")[3:-1].split(","))
+    else:
+        t_bounds = (0,1)
+    expr_x, expr_y = [sympify(s.strip(", ")) for s in in_str.split("|")]
+    expr_x.subs(Symbol("e"), np.e)
+    expr_x.subs(Symbol("pi"), np.pi)
+    expr_x.subs(Symbol("π"), np.pi)
+    expr_y.subs(Symbol("e"), np.e)
+    expr_y.subs(Symbol("pi"), np.pi)
+    expr_y.subs(Symbol("π"), np.pi)
+    try:
+        fx = lambdify(Symbol("t"), expr_x, modules=numpy)
+        fy = lambdify(Symbol("t"), expr_y, modules=numpy)
+        t = np.linspace(*t_bounds, 400)
+    except:
+        fx = np.vectorize(lambda t: expr_x.subs(Symbol("t"), t))
+        fy = np.vectorize(lambda t: expr_y.subs(Symbol("t"), t))
+        t = np.linspace(*t_bounds, 100)
+    x = fx(t)
+    y = fy(t)
+    for i in range(len(t)):
+        try:
+            x[i] = float(x[i])
+        except:
+            x[i] = np.NaN
+        try:
+            y[i] = float(y[i])
+        except:
+            y[i] = np.NaN
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, polar=polar)
+    if not polar:
+        ax.spines['top'].set_visible(False)#.set_color('white') 
+        ax.spines['right'].set_visible(False)#.set_color('white')
+        ax.spines['left'].set_color('white')
+        ax.spines['bottom'].set_color('white')
+        ax.tick_params(axis='x', colors='white')
+        ax.tick_params(axis='y', colors='white')
+        ax.grid(True)
+        #ax.axis("equal")
+        if x_bounds is not None:
+            ax.set(xlim=x_bounds)
+        if y_bounds is not None:
+            ax.set(ylim=y_bounds)
+        ax.spines['left'].set_position(('data', min(max(ax.get_xlim()[0], 0), ax.get_xlim()[1])))
+        ax.spines['bottom'].set_position(('data', min(max(ax.get_ylim()[0], 0), ax.get_ylim()[1])))
+    else:
+        for spine in ax.spines.values():
+            spine.set_color('white')
+            ax.tick_params(colors='white')
+            label_positions = ax.get_xticks()
+            xL=['0',r'$\frac{\pi}{4}$',r'$\frac{\pi}{2}$',r'$\frac{3\pi}{4}$',\
+                r'$\pi$',r'$\frac{5\pi}{4}$',r'$\frac{3\pi}{2}$',r'$\frac{7\pi}{4}$']
+            ax.set_xticklabels(xL)
+    ax.plot(x, y)
     buf = io.BytesIO()
     buf.name = "graph.png"
     fig.savefig(buf, format='png', transparent=True)
