cf28d59fbb21e5a7c350a1e1d02ca44998873d4f
Yakov <yakov.shalunov@gmail.com>
2020-12-13 22:10:12 -0800

    sadness

--- quack.py
+++ quack.py
@@ -15,7 +15,8 @@ try:
         print(f"launching {module}")
         processes[module] = subprocess.Popen([sys.executable, module], stderr=log_files[module], stdout=log_files[module])
     while True:
-        pull_attempt = str(subprocess.check_output(f"git --git-dir={os.path.dirname(os.path.abspath(__file__))}/.git pull", shell=True))[2:-1]
+        #pull_attempt = str(subprocess.check_output(f"git --git-dir={os.path.dirname(os.path.abspath(__file__))}/.git pull", shell=True))[2:-1]
+        pull_attempt = str(subprocess.check_output(f"git pull", shell=True))[2:-1]
         if "Already up to date." not in pull_attempt:
             print("Pulled something.")
             if sys.argv[0] in pull_attempt:
